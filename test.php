fa425799da6c5ce1ef86ae2c55f8802aa4253c49
Alain Kelleter <alain.kelleter@gmail.com>
2024-11-26 09:02:31 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+ echo 'test';
+ echo '<br>';
+ echo 'un' . ' ' . 'deux';
+ echo'<br>';
+ $var1 = "Mon projet";
+ echo $var1;
+ echo'<br>';
+ $var1 .= " une autre chaîne";
+ echo $var1;
+ echo'<br>';
